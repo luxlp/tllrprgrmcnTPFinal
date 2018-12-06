95fa37ca10da903c19d829efc1575b2683f58153
lucas la pietra <lux_lp@hotmail.com>
2018-12-06 16:27:46 -0300

    a

---
+++ UnitTestTPFinal/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTPFinal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTPFinal")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f068ad6b-36ba-475e-a33b-6cb1879dc4f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestTPFinal/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTPFinal
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            TPFinal.Fachada iFachada = new TPFinal.Fachada();
+            string DNI = "12345678";
+            string Pass = "1234";
+            
+        }
+    }
+}
